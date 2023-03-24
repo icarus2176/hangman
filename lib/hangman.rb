9b9c9f37bff083274ea17566649f8f6db9eb7082
icarus2176 <ashe.kemuri@gmail.com>
2023-03-24 12:49:42 -0500

    add save files

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,21 +1,44 @@
+require "csv"
+
 module Hangman
 
   class Game
-    def play_game
-      @word = new_game
-      @wrong_guesses = 6
+    def start
+      saves = CSV.open("saves.csv")
+
+      @saves_number = saves.readlines.size - 1
+
+      if @saves_number > 0
+        puts "Would you like to load a save file? y/n"
+        answer = gets.chomp.downcase
+        if answer == "y"
+          puts "You have #{@saves_number}, enter a number."
+          answer = gets.chomp.downcase
+          load_game(answer.to_i)
+        end
+      end
+      new_game
+    end
+
+    def new_game
+      @word = create_word
+      @remaining_guesses = 6
       @correct_letters = []
       @wrong_letters = []
+      
+      play_game
+    end
 
-      until @wrong_guesses == 0
-        @wrong_guesses -= turn
+    def play_game 
+      puts "At any point type save then press enter to save your game."
+      until @remaining_guesses == 0
+        @remaining_guesses -= turn
       end
       lose
-    end
-    
+    end 
     private
 
-    def new_game 
+    def create_word 
       word = ""
       until word.length.between?(5, 12)
         random_line = rand(10000) - 1
@@ -35,7 +58,9 @@ module Hangman
       puts "Guess a letter then press enter."
       guess = gets.chomp.downcase
 
-      if @word.include?(guess)
+      if guess == "save"
+        save_game
+      elsif @word.include?(guess)
         @correct_letters.push(guess)
         if check_win
           win
@@ -59,7 +84,7 @@ module Hangman
       puts result
       puts "Correct guesses: #{@correct_letters}"
       puts "Wrong guesses: #{@wrong_letters}"
-      puts "You have #{@wrong_guesses} wrong guesses remaining."
+      puts "You have #{@remaining_guesses} wrong guesses remaining."
     end
 
     def win
@@ -90,9 +115,47 @@ module Hangman
         play_game
       end
     end
+
+    def save_game
+
+      saves = CSV.open(
+        "saves.csv",
+        "r+",
+        headers: true,
+        header_converters: :symbol
+      )
+      for i in 0..@saves_number 
+        saves.readline
+      end
+      @saves_number += 1
+      saves << [@saves_number, @word.join, @remaining_guesses,
+        @correct_letters.join, @wrong_letters.join]
+      
+      puts "Save sucessful."
+      turn
+    end
+
+    def load_game(save_number)
+      saves = CSV.open(
+        "saves.csv",
+        "r",
+        headers: true,
+        header_converters: :symbol
+      )
+
+      for i in 1..save_number
+        save_file = saves.readline
+      end
+
+      @word = save_file[1].split("")
+      @remaining_guesses = save_file[2]
+      @correct_letters = save_file[3].split("")
+      @wrong_letters = save_file[4].split("")
+      puts @word.class
+      play_game
+    end
   end
 
   game = Game.new
-  game.play_game
+  game.start
 end
-
